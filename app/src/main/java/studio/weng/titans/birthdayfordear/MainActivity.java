3f76d0013c70403bb12943690c0887595c6ca10c
TitansWeng <ms0312430@gmail.com>
2016-10-25 14:59:21 +0800

    Neeeeeeeeeeeeewwwwwwwwwwww

--- app/src/main/java/studio/weng/titans/birthdayfordear/MainActivity.java
+++ app/src/main/java/studio/weng/titans/birthdayfordear/MainActivity.java
@@ -1,12 +1,41 @@
 package studio.weng.titans.birthdayfordear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                Intent intent = new Intent();
+
+                intent.setClass(MainActivity.this, ScrollingActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+            case MotionEvent.ACTION_OUTSIDE:
+                break;
+        }
+
+
+        return super.onTouchEvent(event);
+    }
+    
 }
+
